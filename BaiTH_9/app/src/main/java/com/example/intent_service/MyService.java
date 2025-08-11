d02cfdfa8bb7dfaf576f91dfeb33d4d487e52732
XuanTruong158 <2251172535@e.tlu.edu.vn>
2025-08-12 02:30:45 +0700

    add bai 9

---
+++ BaiTH_9/app/src/main/java/com/example/intent_service/MyService.java
@@ -0,0 +1,56 @@
+package com.example.intent_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    private MediaPlayer mymedia;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mymedia = MediaPlayer.create(this, R.raw.trentinhbanduoitinhyeu);
+        mymedia.setLooping(true); // Lặp lại
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent != null && intent.getAction() != null) {
+            switch (intent.getAction()) {
+                case "ACTION_PLAY":
+                    if (!mymedia.isPlaying()) {
+                        mymedia.start();
+                    }
+                    break;
+
+                case "ACTION_STOP":
+                    if (mymedia.isPlaying()) {
+                        mymedia.stop();
+                        mymedia.release();
+                        mymedia = MediaPlayer.create(this, R.raw.trentinhbanduoitinhyeu);
+                    }
+                    break;
+            }
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mymedia != null) {
+            if (mymedia.isPlaying()) {
+                mymedia.stop();
+            }
+            mymedia.release();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
