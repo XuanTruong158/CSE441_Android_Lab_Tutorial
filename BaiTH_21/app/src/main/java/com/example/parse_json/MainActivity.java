628196bf0b4c22efb2b5ea72120e426c87146f8c
XuanTruong158 <2251172535@e.tlu.edu.vn>
2025-08-16 04:19:12 +0700

    add bai 21

---
+++ BaiTH_21/app/src/main/java/com/example/parse_json/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.parse_json;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnparse;
+    ListView lv;
+    ArrayList<String> mylist;
+    ArrayAdapter<String> myadapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnparse = findViewById(R.id.btnparse);
+        lv = findViewById(R.id.lv);
+        mylist = new ArrayList<>();
+        myadapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        lv.setAdapter(myadapter);
+        btnparse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parsejson();
+            }
+        });
+    }
+
+    private void parsejson() {
+        String json = null;
+        try {
+            InputStream inputStream = getAssets().open("computer.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+            JSONObject reader = new JSONObject(json);
+            mylist.add(reader.getString("MaDM"));
+            mylist.add(reader.getString("TenDM"));
+
+            JSONArray myArray = reader.getJSONArray("Sanphams");
+            for (int i = 0; i < myArray.length(); i++) {
+                JSONObject myobj = myArray.getJSONObject(i);
+                mylist.add(myobj.getString("MaSP") + " - " +
+                        myobj.getString("TenSP") + " - " +
+                        myobj.getString("SoLuong") + " - " +
+                        myobj.getString("DonGia") + " - " +
+                        myobj.getString("ThanhTien") + " - " +
+                        myobj.getString("Hinh"));
+            }
+            myadapter.notifyDataSetChanged();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (JSONException e2) {
+            e2.printStackTrace();
+        }
+    }
+}
